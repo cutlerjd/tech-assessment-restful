e046292d600e82adc8ee827a8db8607d1e2d3196
Jase Cutler <jasecutler@gmail.com>
2019-05-30 13:02:28 -0700

    Dist commited before git ignore --need to remove

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
+const ActivityLogging_1 = __importDefault(require("./middleware/ActivityLogging"));
+const ActivityRouter_1 = __importDefault(require("./routes/ActivityRouter"));
 const UserRouter_1 = __importDefault(require("./routes/UserRouter"));
 const app = express_1.default();
 const port = 8080;
@@ -13,7 +15,8 @@ app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
     res.send("hello tech-assessment-restful");
 });
-app.use("/user", UserRouter_1.default);
+app.use("/user", ActivityLogging_1.default, UserRouter_1.default);
+app.use("/activity", ActivityRouter_1.default);
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`server started at http://localhost:${port}`);
--- dist/routes/UserRouter.js
+++ dist/routes/UserRouter.js
@@ -4,9 +4,47 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const User_1 = require("../classes/User");
+const data_1 = __importDefault(require("../stores/data"));
 const router = express_1.default.Router();
+router.post("/", (req, res) => {
+    const user = new User_1.User(req.body);
+    const errors = data_1.default.addUser(user);
+    if (errors.length === 0) {
+        res.send("User added");
+    }
+    else {
+        res.send(errors);
+    }
+});
+router.patch("/", (req, res) => {
+    const user = new User_1.User(req.body);
+    const errors = data_1.default.editUser(user);
+    if (errors.length === 0) {
+        res.send("User edited.");
+    }
+    else {
+        res.send(errors);
+    }
+});
+router.delete("/", (req, res) => {
+    res.send("User deleted.");
+});
+router.get("/:email", (req, res) => {
+    const user = data_1.default.getUserByEmail(req.params.email);
+    if (user == null) {
+        return user;
+    }
+    else {
+        return "User not found.";
+    }
+});
+router.post("/:email/activity", (req, res) => {
+    res.send("Activity posted.");
+});
 router.get("/", (req, res) => {
-    res.send("Hello from user router");
+    const users = data_1.default.users;
+    res.send(users);
 });
 exports.default = router;
 //# sourceMappingURL=UserRouter.js.map
