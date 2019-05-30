242feaf05dbde07e439141fb13da3f56e3d00a57
Jase Cutler <jasecutler@gmail.com>
2019-05-30 13:03:07 -0700

    Fixed next issue

--- src/middleware/ActivityLogging.ts
+++ src/middleware/ActivityLogging.ts
@@ -1,8 +1,7 @@
 import { Request, Response} from "express";
 
 function ActivityLogger(req: Request, res: Response, next: any) {
-    // tslint:disable-next-line:no-console
-    console.log( req );
+    next();
 }
 
 export default ActivityLogger;
