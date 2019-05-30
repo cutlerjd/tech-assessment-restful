fbada8521d83de07b3edefc72613dc0b1cc891cd
Jase Cutler <jasecutler@gmail.com>
2019-05-30 13:57:36 -0700

    Added ID so email address could change

--- src/classes/User.ts
+++ src/classes/User.ts
@@ -1,15 +1,18 @@
 interface IUser {
+    id: number;
     email?: string;
     password?: string;
     phone?: string;
 }
 
 class User implements IUser {
+    public id: number;
     public email: string;
     public password: string;
     public phone?: string | null;
 
     constructor(u: IUser) {
+        this.id = -1;
         this.email = u.email !== undefined ? u.email : "";
         this.password = u.password !== undefined ? u.password : "";
         this.phone = u.phone !== undefined ? u.phone : null;
