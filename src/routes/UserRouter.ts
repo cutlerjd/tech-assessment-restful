d3a15969baab09e5de597792dde0a3061c0bda9c
Jase Cutler <jasecutler@gmail.com>
2019-05-30 13:03:53 -0700

    Adding needed routes

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,9 +1,49 @@
 import express from "express";
+import {IUser, User} from "../classes/User";
+import userStore from "../stores/data";
 
 const router = express.Router();
 
+router.post("/", (req, res) => {
+    const user: User = new User(req.body as IUser);
+    const errors: string [] = userStore.addUser(user);
+    if (errors.length === 0 ) {
+        res.send("User added");
+    } else {
+        res.send(errors);
+    }
+});
+
+router.patch("/", (req, res) => {
+    const user: User = new User(req.body as IUser);
+    const errors: string [] = userStore.editUser(user);
+    if (errors.length === 0 ) {
+        res.send("User edited.");
+    } else {
+        res.send(errors);
+    }
+});
+
+router.delete("/", (req, res) => {
+    res.send("User deleted.");
+});
+
+router.get("/:email", (req, res) => {
+    const user: User | null = userStore.getUserByEmail(req.params.email);
+    if (user ! == null) {
+        return user;
+    } else {
+        return "User not found.";
+    }
+});
+
+router.post("/:email/activity", (req, res) => {
+    res.send("Activity posted.");
+});
+
 router.get("/", (req, res) => {
-    res.send("Hello from user router");
+    const users: User[] = userStore.users;
+    res.send(users);
 });
 
 export default router;
